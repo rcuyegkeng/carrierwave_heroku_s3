c01aa40888eebcdf5cf193ed3acb718ae19890bc
Robert Cuyegkeng <rcuyegkeng@gmail.com>
2012-06-20 10:27:25 -0400

    Added file size validator.

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,3 +1,8 @@
+require 'file_size_validator'
+
 class Program < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+
+  validates :image,
+            :file_size => { :maximum => 2.megabytes.to_i }
 end
